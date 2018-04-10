62a6067577535db53b4973cf94bb70018d54bb55
James <jamesgupchurch@knights.ucf.edu>
2018-04-09 23:44:23 -0400

    Fixed errors

--- checksum.c
+++ checksum.c
@@ -7,12 +7,39 @@
 #include <string.h>
 
 
-void readFile(char *filename, int size)
+
+
+int bitmask(char *word, int size)
+{
+	int i = 0, mask;
+	mask = word[i];
+	
+	for(; size != 0; size = size - 8)
+	{
+		mask = (mask << 8) + word[i + 1];
+
+		i++;
+	}
+
+	return mask;
+}
+
+void checksum(int size, char *input, int *check)
+{
+	int word, tempCheck;
+
+	word = bitmask(input, size);
+	tempCheck = bitmask(check, size);
+
+	*check = word + tempCheck;
+}
+
+void readFile(int size, char *filename)
 {
 	FILE *fp;
 	int buffer, i = 0, j = 0, characterCnt = 0, wordLen = size / 4 + 1;
 	int *check = calloc(1, int);
-	char *output = malloc(sizeof(char * wordLen));
+	char *output = malloc(sizeof(char) * wordLen);
 
 	if (size != 8 && size != 16 && size != 32)
 	{
@@ -55,35 +82,14 @@ void readFile(char *filename, int size)
 		i++;
 	}
 
-	printf("%2d bit checksum is %8lx for all %4d chars\n", size, check, characterCnt);
+	printf("%2d bit checksum is %8lx for all %4d chars\n", size, *check, characterCnt);
 
 	fclose(fp);
 }
 
-void checksum(int size, char *input, int *check)
-{
-	int word, tempCheck;
 
-	word = bitmask(input, size);
-	tempCheck = bitmask(check, size);
 
-	*check = word + tempCheck;
-}
 
-int bitmask(char *word, int size)
-{
-	int i = 0, mask;
-	mask = word[i];
-	
-	for(size; size != 0; size = size - 8)
-	{
-		mask = (mask << 8) + word[i + 1];
-
-		i++;
-	}
-
-	return mask;
-}
 
 int main(int argc, char *argv[])
 {
